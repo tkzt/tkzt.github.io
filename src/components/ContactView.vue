fb94d0e11ab6c424f77de90347ed47420fc4cf4d
Allen Tao <38272264+tkzt@users.noreply.github.com>
2023-03-11 11:52:35 +0800

    Update ContactView.vue

--- src/components/ContactView.vue
+++ src/components/ContactView.vue
@@ -2,9 +2,9 @@
     <p class="plain font-lg">与君初相识，犹如故人归。嗨，别来无恙啊！</p>
     <p class="divider-filled"></p>
     <ul>
-        <li>Email: <a href="mailto:allen@tkzt.cn" class="text-link">allen@tkzt.cn</a></li>
-        <li>Github: <a href="https://github.com/tkzt" target="_blank" class="text-link">boring-plans</a>
+        <li>Github: <a href="https://github.com/tkzt" target="_blank" class="text-link">tkzt</a>
         </li>
+        <li>Email: <a href="mailto:allen@tkzt.cn" class="text-link">allen@tkzt.cn</a></li>
         <li>Twitter: <a href="https://twitter.com/allentao_o" target="_blank"
                 class="text-link">@allentao_o</a></li>
     </ul>
