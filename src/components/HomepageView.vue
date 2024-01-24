23a7b89d43c55d3ef1d7ac940a90aaba4d53cc10
Allen Tao <allen@tkzt.cn>
2024-01-24 09:59:07 +0800

    feat: updates intro

--- src/components/HomepageView.vue
+++ src/components/HomepageView.vue
@@ -10,11 +10,12 @@
           于 2019 年毕业于<a href="https://www.bing.com/search?q=%E8%A0%A1%E4%B8%93" target="_blank"
             class="link">蠡湖专科<i class="outlink">
             </i></a>，继而成为一名新生代农民工；目前在<a href="https://iot.tcl.com/" target="_blank"
-            class="link">这个车间<i
-              class="outlink"></i></a>上班；生性胆小，爱好和平，不善表达，嗜睡，同时喜爱武侠与科幻；先后在南京、上海、无锡、上海痛恨、改造、亲手堆砌过若干座屎山，艰难积攒财富的同时，轻易地得到了肥胖、肩颈疾病、高血压；是一个<a
+            class="link">这个工地<i
+              class="outlink"></i></a>搬砖；生性胆小，爱好和平，不善表达，嗜睡，同时喜爱武侠与科幻；先后在南京、上海、无锡、上海痛恨、改造、亲手堆砌过若干座屎山，艰难积攒财富的同时，轻易地得到了肥胖、肩颈疾病、高血压；是一个<a
             href="https://fine-weather-gallery.tkzt.cn" target="_blank" class="link">好天气摄影<i
               class="outlink"></i></a>爱好者；胡乱学过一些花拳绣腿，造了一些<router-link to="/boring-plans"
-            class="link">玩具</router-link>；云顶之弈好玩赌狗阵容、饥荒里常选伍迪；由淮扬菜组成，更喜欢苏帮菜，最爱鸡蛋灌饼、煎饼馃子类食物；偶尔心血来潮，写一些<router-link
+            class="link">玩具</router-link>；云顶之弈好玩赌狗阵容、饥荒里常选<span
+            class="line-through">伍迪</span>薇格弗德；由淮扬菜组成，更喜欢苏帮菜，最爱鸡蛋灌饼、煎饼馃子类食物；偶尔心血来潮，写一些<router-link
             to="/boring-blogs"
             class="link">不堪卒读的文章</router-link>；喜欢看岩井俊二、姜文的电影，听宋冬野、逼哥、李宗盛、周杰伦、马飞、王菲和新裤子的歌。
         </div>
